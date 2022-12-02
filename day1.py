b726af1a7b7d17d7bec649b7065ee1ecae392894
Matthew Crowson <hey@matthewcrowson.com>
2022-12-01 19:37:17 -0500

    heap it all

--- day1.py
+++ day1.py
@@ -4,11 +4,10 @@ import heapq
 
 
 sum_of_snacks = [sum([int(c) for c in elf.split("\n")]) for elf in data.split("\n\n")]
+heapq.heapify(sum_of_snacks)
 
-a = max(sum_of_snacks)
+a = heapq.nlargest(1, sum_of_snacks)
 print(a)
 
-heapq.heapify(sum_of_snacks)
 b = sum(heapq.nlargest(3, sum_of_snacks))
-
 print(b)
