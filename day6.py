ab6dac2f99dfb8b4cf46083ad32a97646608c2f7
Matthew Crowson <hey@matthewcrowson.com>
2022-12-06 11:17:45 -0500

    added pointer method

--- day6.py
+++ day6.py
@@ -1,11 +1,65 @@
 from aocd import data
+from collections import defaultdict
 
 
-def get_msg(msg, msg_len):
+def set_it(msg, msg_len):
     for i in range(msg_len, len(msg)):
         if len(set(msg[i - msg_len : i])) == msg_len:
             return i
 
 
-print(get_msg(data, 4))
-print(get_msg(data, 14))
+print("Using Set: ")
+print(set_it(data, 4))
+print(set_it(data, 14))
+
+
+def pointer(msg, msg_len):
+    end = msg_len
+    buffer = msg[:end]
+    counts = defaultdict(int)
+    for ch in buffer:
+        counts[ch] += 1
+
+    while len(counts) != msg_len:
+        counts[buffer[0]] -= 1
+        if counts[buffer[0]] == 0:
+            del counts[buffer[0]]
+        counts[msg[end]] += 1
+        buffer = buffer[1:] + msg[end]
+        end += 1
+
+    return end
+
+
+print("Using Pointer: ")
+print(pointer(data, 4))
+print(pointer(data, 14))
+
+if __name__ == "__main__":
+    import timeit
+
+    w_sets = timeit.timeit(
+        "set_it(data, 4)",
+        number=1000,
+        setup="from __main__ import set_it; from aocd import data",
+    )
+    w_pointer = timeit.timeit(
+        "pointer(data, 4)",
+        number=1000,
+        setup="from __main__ import pointer; from aocd import data",
+    )
+
+    print("Time with Sets: ", w_sets)
+    print("Time with Pointer: ", w_pointer)
+
+
+"""
+Using Set: 
+1909
+3380
+Using Pointer: 
+1909
+3380
+Time with Sets:  0.40100679197348654
+Time with Pointer:  0.5636653330293484
+"""
