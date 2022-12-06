6e1fa8a298b45cd7264f9f156285e9955e55eb19
Matthew Crowson <hey@matthewcrowson.com>
2022-12-06 14:02:39 -0500

    slightly faster day6

--- day6.py
+++ day6.py
@@ -30,10 +30,26 @@ def pointer(msg, msg_len):
 
     return end
 
-
 print("Using Pointer: ")
 print(pointer(data, 4))
 print(pointer(data, 14))
+
+def decreasing_buffer(msg, msg_len):
+    end = msg_len
+    buffer = msg[:end]
+    
+    while buffer:
+        cmp, buffer = buffer[0], buffer[1:]
+        if cmp in buffer:
+            end += 1
+            buffer = msg[end - msg_len:end] 
+    
+    return end
+
+print("Decreasing Buffer: ")
+print(decreasing_buffer(data, 4))
+print(decreasing_buffer(data, 14))
+
 
 if __name__ == "__main__":
     import timeit
@@ -48,9 +64,15 @@ if __name__ == "__main__":
         number=1000,
         setup="from __main__ import pointer; from aocd import data",
     )
+    w_decreasing_buffer = timeit.timeit(
+        "decreasing_buffer(data, 4)",
+        number=1000,
+        setup="from __main__ import decreasing_buffer; from aocd import data",
+    )
 
     print("Time with Sets: ", w_sets)
     print("Time with Pointer: ", w_pointer)
+    print("Time with Decreasing Buffer: ", w_decreasing_buffer)
 
 
 """
