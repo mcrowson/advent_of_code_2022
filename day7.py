baea9f7ded07c5a2d389c8bcfb054cd16f24fafd
Matthew Crowson <hey@matthewcrowson.com>
2022-12-07 09:12:51 -0500

    day7 done

--- day7.py
+++ day7.py
@@ -1,84 +1,40 @@
 from aocd import data
-from typing import List
-
-data = '''$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k'''
+from collections import defaultdict
+import heapq
 
 
+cmd_hist = data.split("\n")
 
-'''
-def get_dir_size(dir_name: str, parent_path: List=None) -> int:
-    this_size = 0
-    if not parent_path:
-        parent_path = []
-    contents = []
-    
-    # scan input for "cd %"
-    pos = cmd_hist.index(f'$ cd {dir_name}')
-    
-    # skip the ls command
-    pos += 2
-    # get everything up to next $ and that is this dir contents
-    line = cmd_hist[pos]
-    while line[0] != '$' and pos < len(cmd_hist) - 1:
-        print(line)
-        contents.append(line)
-        pos += 1
-        line = cmd_hist[pos]
-    
-    # First recursively call get_dir_sizes on internal directories
-    for c in contents:
-        if c[:4] == 'dir ':
-            this_size += get_dir_size(c[4:], parent_path + [dir_name])
-        else:
-            print(c)
-            fs, _ = c.split(' ')
-            this_size += int(fs)
-    
-    dir_sizes[dir_name] = this_size
-    # save to dir_sizes
-    return this_size
-    
-get_dir_size('/')
-
-print("more")
-print(dir_sizes)
-'''
-
-cmd_hist = data.split('\n')
-
-dir_sizes = {}
-
-tree = {}
+dir_sizes = defaultdict(int)
 
 current_path = []
 for cmd in cmd_hist:
-    if cmd[:4] == '$ cd':
-        current_path.append(cmd[:4])
-    if cmd == '$ ls':
-        
+    if cmd[:4] == "$ cd":
+        if cmd[5:] == "..":
+            current_path.pop(-1)
+        else:
+            current_path.append(cmd[5:])
+    elif "$ " != cmd[:2] and "dir " != cmd[:4]:
+        fs, _ = cmd.split(" ")
+        path = ""
+        for dir in current_path:
+            path += f"/{dir}"
+            dir_sizes[path] += int(fs)
 
 
-a = sum([s for d, s in dir_sizes.items() if s <= 100000]) 
-print(a)   
+a = sum([s for s in dir_sizes.values() if s <= 100000])
+print(a)
+
+desired_space = 70000000 - 30000000
+current_space = dir_sizes["//"]
+need_to_free = current_space - desired_space
+
+possible_deletions = []
+
+for k, v in dir_sizes.items():
+    if v < need_to_free:
+        continue
+    heapq.heappush(possible_deletions, (v, k))
+
+b = heapq.heappop(possible_deletions)
+print(b[0])
