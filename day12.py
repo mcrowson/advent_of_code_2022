7e64f86fb575fd0e3f81521d9d1c380a7e33574d
Matt Crowson <mcrowson@datto.com>
2022-12-12 10:13:39 -0500

    day12 done

--- day12.py
+++ day12.py
@@ -1,44 +1,43 @@
 from aocd import data
-from operator import add
+from typing import List
 from string import ascii_lowercase
 
-data = '''Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi'''
+letter_i = {l: i for i, l in enumerate(ascii_lowercase)}
+letter_i.update({"S": -1, "E": 26})
+data_numeric = [[letter_i[i] for i in row] for row in data.split("\n")]
 
-q = []
-visited = set()
-movement = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-steps = 0
 
-for i, row in enumerate(data.split('\n')):
-    for j, letter in enumerate(row):
-        if letter == 'S':
-            q.append((i, j))
-            
-while q:
-    for _ in range(len(q)):
-        spot = q.pop(0)
-        letter = data[spot[0]][spot[1]]
-        
-        if spot == 'E':
-            print(steps)
-        
-        if letter == 'S':
-            spot_elev = -1
-        else:
-            spot_elev = ascii_lowercase.index(letter)
-        for m in movement:
-            possible = (spot[0] + m[0])
-            if possible in visited:
-                continue
-            pos_letter = data[possible[0]][possible[1]]
-            elev = ascii_lowercase.index(pos_letter)
-            if elev + 1 != spot_elev:
-                continue
-            q.append(possible)
-    steps += 1
-            
-            
+def bfs(dn: List, start: int, end: int) -> int:
+    q = [(i, j) for i in range(len(dn)) for j in range(len(dn[0])) if dn[i][j] == start]
+    visited = set()
+    movement = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    steps = 0
+
+    while q:
+        for _ in range(len(q)):
+            spot = q.pop(0)
+            visited.add(spot)
+            elev = dn[spot[0]][spot[1]]
+
+            if elev == end:
+                return steps
+
+            for m in movement:
+                possible = (spot[0] + m[0], spot[1] + m[1])
+                if (
+                    -1 in possible
+                    or possible[0] > len(dn) - 1
+                    or possible[1] > len(dn[0]) - 1
+                ):
+                    continue
+                if possible in visited or possible in q:
+                    continue
+                pos_elev = dn[possible[0]][possible[1]]
+                if pos_elev > 1 + elev:
+                    continue
+                q.append(possible)
+        steps += 1
+
+
+print(bfs(data_numeric, -1, 26))
+print(bfs(data_numeric, 0, 26))
